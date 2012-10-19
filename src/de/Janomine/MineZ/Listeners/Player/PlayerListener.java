40e2bbaafbb65d8820a542505e911cae87882b85
Ch4t4r <flyffassistman@gmx.de>
2012-10-19 22:13:33 +0200

    Eine Falsch gesetzte Klammer, die immer den ":" ausradierte.

--- src/de/Janomine/MineZ/Listeners/Player/PlayerListener.java
+++ src/de/Janomine/MineZ/Listeners/Player/PlayerListener.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -14,14 +15,14 @@ public class PlayerListener implements Listener {
 	
 	
 	
-		 @EventHandler
+		 @EventHandler(priority = EventPriority.HIGHEST)
 		 public void Funk(AsyncPlayerChatEvent event) {
 			 Player player = event.getPlayer();
 			 String nachricht = event.getMessage();
 			 String Playername = player.getName();
 			 
 			 if (player.getItemInHand().getType() == Material.BOOK_AND_QUILL) {
-				 event.setFormat(player.getDisplayName().replaceAll(":", ChatColor.GREEN + "[Funk] : ") + nachricht);
+				 event.setFormat(player.getDisplayName().replaceAll(":", ChatColor.GREEN + "[Funk] : " + nachricht));
 				 return;
 			 }
 			 
