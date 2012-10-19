d4047dc43d16e6741679b378235f6c0a258fca5f
Janomine <Janomine.s@googlemail.com>
2012-10-18 18:29:26 -1200

    Rucksack grunladen Gefixt. Bin nun offline. Zeit 18:30

---
+++ src/de/Janomine/MineZ/Listeners/Player/RuckSack.java
@@ -0,0 +1,34 @@
+package de.Janomine.MineZ.Listeners.Player;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class RuckSack implements Listener {
+	
+	public void Rucksack(PlayerInteractEvent event) {
+		
+		Player player = event.getPlayer();
+		String Playername = player.getName();
+		
+		
+		
+		 if (player.getItemInHand().getType() == Material.CHEST) {
+			 if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+		 }
+		if(!player.hasPermission("MineZ.Rucksack")) {
+			event.setCancelled(true);
+			return;
+			
+			
+			
+		}
+		player.sendMessage("§8Rucksack geöffnet.");
+		
+	}
+
+}
+	
+}
