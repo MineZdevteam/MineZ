4872d7a4f0f016789d98a43c9caaab20c3939f3c
Ch4t4r <flyffassistman@gmx.de>
2012-10-19 20:49:02 +0200

    Explosion verstärkt. TODO: Zerstörung von Blöcken durch Explosion verhindern! Ich gucke jetzt Herr der Ringe, bis morgen ;D

--- src/de/Janomine/MineZ/Listeners/Player/BewegungsListener.java
+++ src/de/Janomine/MineZ/Listeners/Player/BewegungsListener.java
@@ -13,7 +13,7 @@ public class BewegungsListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerMoveEvent(PlayerMoveEvent e){
-		if(e.getPlayer().getWorld().getTime() >= 1900){e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,2000,11));	}else{e.getPlayer().removePotionEffect(PotionEffectType.BLINDNESS);}
+		if(e.getPlayer().getWorld().getTime() >= 12500){e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,2000,11));	}else{e.getPlayer().removePotionEffect(PotionEffectType.BLINDNESS);}
 		
 	}
 }
