2897424844047ad80776e3c21e36eb21bc6c50a8
Janomine <Janomine.s@googlemail.com>
2012-10-18 16:21:58 -1200

    BlockListener geändert.

--- src/de/Janomine/MineZ/Listeners/BuildListener.java
+++ src/de/Janomine/MineZ/Listeners/BuildListener.java
@@ -15,44 +15,7 @@ import org.bukkit.inventory.ItemStack;
 public class BuildListener implements Listener {
 	
 
-	  @EventHandler
-	  public void onBreak(BlockBreakEvent event) {
-	   
-	      try
-	      {
-	        Player p = event.getPlayer();
-	        if (((p.getItemInHand().getType() == Material.IRON_SPADE) && (event.getBlock().getType() == Material.WEB)) || 
-	          ((p.getItemInHand().getType() == Material.WOOD_HOE) && (event.getBlock().getType() == Material.MELON_BLOCK)) || 
-	          ((p.getItemInHand().getType() == Material.WOOD_HOE) && (event.getBlock().getType() == Material.BROWN_MUSHROOM)) || 
-	          ((p.getItemInHand().getType() == Material.WOOD_HOE) && (event.getBlock().getType() == Material.RED_MUSHROOM)) || 
-	          (event.getBlock().getType() == Material.CAKE_BLOCK) || (event.getBlock().getType() == Material.CAKE))
-	        {
-	          if (event.getBlock().getType() == Material.WEB) event.getBlock().setType(Material.AIR);
-	          if (event.getBlock().getType() == Material.MELON_BLOCK) { event.getBlock().setType(Material.AIR); dropMelons(p, event.getBlock().getLocation()); }
-	        } else {
-	          event.setCancelled(true);
-	        }
-	      } catch (Exception ex) { event.setCancelled(true); }
-	    }
-	  
-
-	  @EventHandler
-	  public void onPlace(BlockPlaceEvent event) {
-		
-			 
-			
-	    Player player = event.getPlayer();
-	    GameMode gm = player.getGameMode();
-	        if(event.getBlock().getType() != Material.WEB && (event.getBlock().getType() != Material.CACTUS &&  (event.getBlock().getType() != Material.CAKE_BLOCK && (event.getBlock().getType() != Material.STONE_BUTTON && (!(gm == GameMode.CREATIVE)))))) { 
-	          event.setCancelled(true);
-	          
-	        } else {
-				
-				event.setCancelled(false);
-			}
-	  }
-	        
-	  
+	
 
    	  
 	  
