f763142f9c916dacf427c5f4a575feb1a460edb8
Janomine <Janomine.s@googlemail.com>
2012-10-18 17:15:50 -1200

    Joinlistener editiert.

---
+++ src/de/Janomine/MineZ/Listeners/JoinListener.java
@@ -0,0 +1,28 @@
+package de.Janomine.MineZ.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class JoinListener implements Listener {
+	
+	@EventHandler
+	public void Join(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		player.sendMessage(ChatColor.GREEN + "MineZ");
+		player.sendMessage(ChatColor.RED + "Willkommen auf dem Deutschen " + ChatColor.GREEN + "MineZ " + ChatColor.RED + "Server.");
+		player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 170, 3));
+	    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 80, 1));
+	    player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 70, 3));
+		
+	}
+
+	
+	
+	}
+
