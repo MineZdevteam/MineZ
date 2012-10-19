5c841fccc18d36b18527cc6b9a9922ed60718476
Ch4t4r <flyffassistman@gmx.de>
2012-10-19 20:35:12 +0200

    Explosion von der Stärke eines Halben TNT's wenn ein Ei auf dem Boden aufkommt.

---
+++ src/de/Janomine/MineZ/Listeners/Entitys/ProjectileTrifftListener.java
@@ -0,0 +1,15 @@
+package de.Janomine.MineZ.Listeners.Entitys;
+
+import org.bukkit.entity.Egg;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class ProjectileTrifftListener {
+
+	@EventHandler
+	public void onProjectileHitEvent(ProjectileHitEvent e){
+		if(e.getEntity() instanceof Egg){
+			e.getEntity().getWorld().createExplosion(e.getEntity().getLocation(),2F);
+		}
+	}
+}
