a68c62efce4778892a94c0e6c659d3fe36733412
Ch4t4r <flyffassistman@gmx.de>
2012-10-20 09:44:45 +0200

    Soo :) Zombies sind jetzt endgültig stärker, hab da was vergessen ;D

--- src/de/Janomine/MineZ/Listeners/Entitys/EntityByEntityListener.java
+++ src/de/Janomine/MineZ/Listeners/Entitys/EntityByEntityListener.java
@@ -16,9 +16,8 @@ public class EntityByEntityListener implements Listener {
 		if(e.getEntity().getType() == EntityType.ZOMBIE  && e.getDamager() instanceof Player){
 			
 			Zombie z = (Zombie) e.getEntity();
-			int l = z.getHealth();
-			Bukkit.broadcastMessage("Zombie Leben " + z.getHealth());
-			if(!(z.getHealth() + 2 > z.getMaxHealth()) && !((z.getHealth() + 1) > l)){
+			int l = z.getHealth() + e.getDamage();
+			if(!(z.getHealth() + 2 > z.getMaxHealth()) && !((z.getHealth() + 2) > l)){
 				z.setHealth(z.getHealth() + 2);
 			}
 		}
