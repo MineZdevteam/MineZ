caf097e41c3d6c21408e4b3f0b2fa7bddf2bb17f
Ch4t4r <flyffassistman@gmx.de>
2012-10-20 10:26:31 +0200

    Ein Paar verbessernungen ; Kein NPE mehr, er setzt trotzdem nicht alle blöcke zurück

--- src/de/Janomine/MineZ/Listeners/World/ExplosionsListener.java
+++ src/de/Janomine/MineZ/Listeners/World/ExplosionsListener.java
@@ -1,6 +1,8 @@
 package de.Janomine.MineZ.Listeners.World;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -10,8 +12,9 @@ public class ExplosionsListener implements Listener{
 	@EventHandler
 	public void onExplosionEvent(EntityExplodeEvent e){
 		for(int i = 0;i < e.blockList().size();i++){
-			Bukkit.broadcastMessage(e.blockList().get(i).toString());
-
+			Location loc = new Location(Bukkit.getWorld("asdf2"),e.blockList().get(i).getLocation().getX(),e.blockList().get(i).getLocation().getY(),e.blockList().get(i).getLocation().getZ());
+			Material type = e.blockList().get(i).getType();
+			Bukkit.getWorld("asdf2").getBlockAt(loc).setType(type);
 
 		}
 	}
